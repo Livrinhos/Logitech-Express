acb4fa2b30f5855c9e999484de1de7fffa9d7056
Livrinho <wolfgamer0107@gmail.com>
2026-06-23 14:48:21 -0300

    Add vehicle routes to the API

--- src/routes/veiculoRoutes.js
+++ src/routes/veiculoRoutes.js
@@ -1,2 +1,14 @@
 // Define as rotas da API relacionadas aos veículos.
 // Organiza os endpoints utilizados pelo sistema.
+// Define as rotas da API relacionadas aos veículos.
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/veiculos');
+router.get('/veiculos/:id');
+router.post('/veiculos');
+router.put('/veiculos/:id');
+router.delete('/veiculos/:id');
+
+module.exports = router;
